97551fca1e0b6d37b3dbb958e6a76c761f1a2a58
Yeudanta Mahardika Aditya Wicaksana <159878204+yuudantaa@users.noreply.github.com>
2025-09-22 22:11:24 +0700

    commit

---
+++ TodoListHybrid/TodoListHybrid/TodoListHybrid.Web.Client/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using TodoListHybrid.Shared.Services;
+
+namespace TodoListHybrid.Web.Client.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "WebAssembly";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
